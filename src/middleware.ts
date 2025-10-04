a33b82c059adc9d3edf52e2647817748a10190f5
Robert Sternberg <rob@robbash.com>
2025-10-04 16:34:35 +0200

    Redirect to detected locale from other pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
-import createMiddleware from 'next-intl/middleware'
-export default createMiddleware({ locales:['en','de'], defaultLocale:'en' })
-export const config = { matcher:['/','/(en|de)/:path*'] }
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({ locales: ['en', 'de'], defaultLocale: 'en' });
+
+export const config = {
+  matcher: ['/((?!_next|.well-known|favicon.ico|.*\\.png|assets|api).*)'],
+};
